683167e45201c4a5ea7921e8fbf46c2ae2ba5aa5
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-06 15:06:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send("article one is requested and will be served here");
 });
-app.get('article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send("article two is requested and will be served here");
 });
-app.get('article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send("article three is requested and will be served here");
 });
 
