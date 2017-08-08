c93491ef2ec34101b6452e18afffd30aac540add
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-08 22:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 
-
+// changed the text of main-text div
 element.innerHTML='new value';
+//move the image
+var img=document.getElementById('madi');
+madi.onclick=function(){
+    madi.style.marginleft='110px';
+}
