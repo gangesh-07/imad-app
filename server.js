73af6be8cc9ad3420f7d832a85f5593c958227ef
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-18 00:46:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Pool=require('pg').Poll;
+var config={
+    user:'gangesh97dhar',
+    database:'gangesh97dhar',
+    host:'db.imad.hasura-app.io'
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
 var articles={
 'article-one':{
     title: 'Article One | Gangesh Dhar',
@@ -97,6 +106,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
+app.ger('/test-db,function(req,res'){
+    
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringfy(result));
+        }
+    });
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
