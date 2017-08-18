fcdb98cb35e994b987740551c064f67d8b77e92f
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-18 07:00:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var Pool=require('pg').Poll;
 var config={
     user:'gangesh97dhar',
     database:'gangesh97dhar',
-    host:'db.imad.hasura-app.io'
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
