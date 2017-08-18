d652295488dcf369b7927906802295b8561179c1
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-18 13:06:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ res.send(JSON.stringify(result.rows));
 
 app.get('/articles/:articleName', function (req, res) {
     
-    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName +"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName +"'",function(err,result) {
         if(err){
             res.status(500).send("Error in getting records from DB"+err.toString());
         }else{
@@ -140,7 +140,7 @@ app.get('/articles/:articleName', function (req, res) {
                 res.status(404).send('Article not found');
             }else{
                var articleData=result.rows[0];
-             res.send(createTemplate(articles[articleData]));
+             res.send(createTemplate(articleData));
             }
             
         }
