331b2ba02f6d6dd007797c1b6cf7d01c3e992e36
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-30 19:30:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,6 +141,37 @@ app.post('/create-user', function (req, res) {
   });
 });
 
+app.post('/login', function (req, res) {
+    //json
+    //
+  var username=req.body.username;
+  var password=req.body.password;
+  
+  
+  pool.query('SELECT * FROM "user" username=$1',[username],(err,result)=>{
+       if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(403).send('Username/Password is Invalid');
+            }else{
+                //match the password
+                var dbString=result.rows[0].password;
+                var salt=dbString.split('$')[2];
+                var hashedPassword=hash(password,salt);
+                if(hashedPassword===dbString)
+                {
+                    res.send('credentials are correct!! ');
+                }else{
+                    res.status(403).send('Username/Password is Invalid');
+                }
+            }
+        }
+        
+      
+  });
+});
+
 
 
 var pool=new Pool(config);
