8bf95802e840845b857cb9b5270bd2feee188a0c
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-31 00:54:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,8 +8,10 @@ var img=document.getElementById('madi');
 
 var marginLeft=0;
 function moveRight() {
+    if(marginLeft<600){
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+"px";
+    }
 }
 
 img.onclick=function(){
