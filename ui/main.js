c9777bf5cb6be76f4deaa8e49879fc7ef73acc09
gangesh-07 <gangesh97dhar@gmail.com>
2017-08-30 20:00:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
+//var element=document.getElementById('main-text');
 
 // changed the text of main-text div
-element.innerHTML='new value';
+//element.innerHTML='new value';
 //move the image
 var img=document.getElementById('madi');
 
@@ -14,4 +14,38 @@ function moveRight() {
 
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
+};
+
+
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+    //create a request object
+    var request=new XMLHttpRequest();
+    
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+      if(request.readystate===XMLHttpRequest.Done)
+      {
+          //take some action
+          if(request.status===200){
+              alert('Logged in successfully!');
+          }else if(request.status===403){
+              alert('Invalid username/password');
+          }else if(request.status===500){
+              alert('something went wrong on the server...sorry');
+          }
+      }
+        
+    };
+    
+   var username=document.getElementById('username');
+   var password=document.getElementById('password');
+   console.log(username);
+   console.log(password);
+   request.open('POST','http://gangesh97dhar.imad.hasura-app.io/login',true);
+   request.setRequestheader('Current-Type','application/json');
+   request.send(JSON.stringify({username: username, password: password}));
+    
 };
